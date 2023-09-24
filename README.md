d906757cbe24f79fb354aa1583127709eb63aa37
LeenHawk <leenhawk@leenhawk.com>
2023-09-24 12:52:19 +0800

    add clang support

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # Professional C++ 5th edition answer
 
 这是对于《Professional C++》第五版的一些自己做的答案（可能借鉴了一点点原书答案），采用MSVC最新支持的C++语法。
+
+## MSVC
+
+主要使用，均可运行。
+
+## GCC
+
+不支持
+
+## Clang
+
+简单测试，未完全覆盖，需要自行编译安装 libc++ module(libstdc++ 没有 module)。
+
+过程为：
+
+```shell
+git clone https://github.com/llvm/llvm-project.git
+cd llvm-project
+mkdir build
+cmake -G Ninja -S runtimes -B build -DLIBCXX_ENABLE_STD_MODULES=ON -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind"
+ninja -C build
+```
+
+记 `build` 文件夹为 `<build>`
+
+
+编译指令为在项目同级文件夹输入:
+
+```shell
+mkdir build
+cmake -G Ninja -S . -B build -DCMAKE_CXX_COMPILER=<path-to-clang> -DLIBCXX_BUILD=<build>
+ninja -C build
+build/main
+```
--- module.cmake
+++ module.cmake
@@ -1,10 +1,14 @@
 CMAKE_MINIMUM_REQUIRED(VERSION 3.25)
 
+set(CMAKE_CXX_STANDARD 23)
+
 # module is the experimental function. cmake experimental api needs to be enabled
 if (CMAKE_VERSION EQUAL 3.25)
     set(CMAKE_EXPERIMENTAL_CXX_MODULE_CMAKE_API "3c375311-a3c9-4396-a187-3227ef642046")
 elseif (CMAKE_VERSION EQUAL 3.26)
     set(CMAKE_EXPERIMENTAL_CXX_MODULE_CMAKE_API "2182bf5c-ef0d-489a-91da-49dbc3090d2a")
+elseif (CMAKE_VERSION EQUAL 3.27)
+    set(CMAKE_EXPERIMENTAL_CXX_MODULE_CMAKE_API "aa1f7df0-828a-4fcd-9afc-2dc80491aca7")
 else()
     message(WARNING "Newer CMake version, CMAKE_EXPERIMENTAL_CXX_MODULE_CMAKE_API may not be set.")
 endif()
@@ -15,8 +19,38 @@ set(CMAKE_EXPERIMENTAL_CXX_MODULE_DYNDEP ON)
 # Clang need to disable c++ extension
 if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
     set(CMAKE_CXX_EXTENSIONS OFF)
+    include(FetchContent)
+    FetchContent_Declare(
+        std
+        URL "file://${LIBCXX_BUILD}/modules/c++/v1/"
+        DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+    )
+    FetchContent_GetProperties(std)
+    if(NOT std_POPULATED)
+        FetchContent_Populate(std)
+        add_subdirectory(${std_SOURCE_DIR} ${std_BINARY_DIR} EXCLUDE_FROM_ALL)
+    endif()
+
+    #
+        # Adjust project compiler flags
+    #
+
+        add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-fprebuilt-module-path=${CMAKE_BINARY_DIR}/_deps/std-build/CMakeFiles/std.dir/>)
+        add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-nostdinc++>)
+        # The include path needs to be set to be able to use macros from headers.
+        # For example from, the headers <cassert> and <version>.
+        add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-isystem>)
+        add_compile_options($<$<COMPILE_LANGUAGE:CXX>:${LIBCXX_BUILD}/include/c++/v1>)
+
+    #
+    # Adjust project linker flags
+    #
+
+    add_link_options($<$<COMPILE_LANGUAGE:CXX>:-nostdlib++>)
+    add_link_options($<$<COMPILE_LANGUAGE:CXX>:-L${LIBCXX_BUILD}/lib>)
+    add_link_options($<$<COMPILE_LANGUAGE:CXX>:-Wl,-rpath,${LIBCXX_BUILD}/lib>)
+    # Linking against std is required for CMake to get the proper dependencies
+    link_libraries(std c++)
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
     set_property(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY VS_STARTUP_PROJECT ${CMAKE_PROJECT_NAME})
 endif()
-
-set(CMAKE_CXX_STANDARD 23)
